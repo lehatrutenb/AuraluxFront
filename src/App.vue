9e24e78fd86d654c036239ac3636a42affd35180
Алексей Маренков <lehatrutenb@MacBook-Pro-Aleksej.local>
2021-06-27 13:58:57 +0300

    Add routes and login form

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,300&display=swap');
+  body { 
+    margin: 0;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Router from 'vue-router'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+import 'boxicons'
+import 'boxicons/css/boxicons.min.css'
+import Login from '@/components/Login'
+import BattleView from '@/components/BattleView'
+import BattleInfo from '@/components/BattleInfo'
+import Battles from '@/components/Battles'
+import Maps from '@/components/Maps'
+import Strategies from '@/components/Strategies'
+
+Vue.use(Router)
+Vue.use(Vuesax)
+Vue.config.productionTip = false
+
+const router = new Router({
+  routes: [
+      { path: '/', component: Login },
+      { path: '/battle/:battle_id/:run_id', component: BattleView },
+      { path: '/battle/:battle_id/', component: BattleInfo },
+      { path: '/battles', component: Battles },
+      { path: '/maps', component: Maps },
+      { path: '/strategies', component: Strategies }
+  ]
+})
+
+new Vue({
+    router,
+    render: h => h(App),
+    components: { App },
+}).$mount('#app')
